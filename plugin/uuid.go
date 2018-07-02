306935aef7dc960bc40b4e40bfaef5b1300ccea0
Andrii Ursulenko <a.ursulenko@gmail.com>
2018-07-02 11:49:57 +0200

    Match zero or one

--- plugin/uuid.go
+++ plugin/uuid.go
@@ -10,7 +10,7 @@ var uuidPattern = "^([a-fA-F0-9]{8}-" +
 	"[%s]" +
 	"[a-fA-F0-9]{3}-" +
 	"[8|9|aA|bB][a-fA-F0-9]{3}-" +
-	"[a-fA-F0-9]{12})*$"
+	"[a-fA-F0-9]{12})?$"
 
 func getUUIDRegex(version int32) (string, error) {
 	if version < 0 || version > 5 {
